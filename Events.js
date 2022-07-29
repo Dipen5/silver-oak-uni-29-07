01ae73317921c23588fc69628cf81a1a57d3fa06
Dipen5 <104885131+Dipen5@users.noreply.github.com>
2022-07-29 14:19:58 +0530

    Add files via upload

---
+++ Events.js
@@ -0,0 +1,23 @@
+function demoOnClick(){
+    alert("On Click");
+}
+
+function demoOnDblClick(){
+    alert("On Double Click");
+}
+
+function demoOnFocus(){
+    document.getElementById("txtFocus").style.backgroundColor="black";
+}
+
+function demoOnBlur(){
+    document.getElementById("txtBlur").style.backgroundColor="red";
+}
+
+function demoOnKeypress(){
+    alert("Keypress Done");
+}
+
+function demoOnKeyUp(){
+    alert("KeyUp Done");
+}
